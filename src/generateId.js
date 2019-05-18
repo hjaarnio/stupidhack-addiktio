ace2b92d1f74a6cc7a5325b99f372b6a13add216
Henrik Aarnio <hjaarnio@gmail.com>
2019-05-18 11:29:42 +0300

    fix ids

--- src/generateId.js
+++ src/generateId.js
@@ -1,42 +1,42 @@
 const fruits = [
-"Apple",
-"Apricot",
-"Avocado",
-"Banana",
-"Blueberry",
-"Blackberry",
-"Cherry",
-"Cranberry",
-"Cucumber",
-"Eggplant",
-"Grape",
-"Grapefruit",
-"Honeydew",
-"Kiwi",
-"Lemon",
-"Lime",
-"Mango",
-"Nectarine",
-"Olive",
-"Orange",
-"Papaya",
-"Peach",
-"Pear",
-"Pineapple",
-"Plum",
-"Pomegranate",
-"Raspberry",
-"Strawberry",
-"Tangerine",
-"Tomato",
-"Watermelon",
+"apple",
+"apricot",
+"avocado",
+"banana",
+"blueberry",
+"blackberry",
+"cherry",
+"cranberry",
+"cucumber",
+"eggplant",
+"grape",
+"grapefruit",
+"honeydew",
+"kiwi",
+"lemon",
+"lime",
+"mango",
+"nectarine",
+"olive",
+"orange",
+"papaya",
+"peach",
+"pear",
+"pineapple",
+"plum",
+"pomegranate",
+"raspberry",
+"strawberry",
+"tangerine",
+"tomato",
+"watermelon",
 ]
 
 export default () => {
   return (
-    fruits[Math.floor(fruits.length * Math.random())] +
-    fruits[Math.floor(fruits.length * Math.random())] +
-    fruits[Math.floor(fruits.length * Math.random())] +
+    fruits[Math.floor(fruits.length * Math.random())] + '.' +
+    fruits[Math.floor(fruits.length * Math.random())] + '.' +
+    fruits[Math.floor(fruits.length * Math.random())] + '.' +
     fruits[Math.floor(fruits.length * Math.random())]
   )
 }
