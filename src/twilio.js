f81ae9b35e18651539303c602b3088eff1460cec
Henrik Aarnio <hjaarnio@gmail.com>
2019-05-18 10:55:58 +0300

    asdf

--- src/twilio.js
+++ src/twilio.js
@@ -1,13 +1,13 @@
 import twilio from 'twilio';
 const URL = process.env.URL || "";
 const TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID || "";
-const TWILIO_AUTH_TOKEN = process.env.TWILIO_ACCOUNT_SID || "";
+const TWILIO_AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN || "";
 
+console.log(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 var client = new twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
 
 function call(number, id){
   console.log('calling number', number, id);
-  return;
   client.calls
     .create({
       url: `${URL}/task/${id}`,
